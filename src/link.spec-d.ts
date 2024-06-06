3dc6484417b66b74fe8b9989b52c82c57f8ca594
Vladimir Ivakin <vla.ivakin@gmail.com>
2024-06-06 22:50:50 +0200

    Add pipe function

--- src/link.spec-d.ts
+++ src/link.spec-d.ts
@@ -1,7 +1,7 @@
 import { Event, createEvent } from "effector";
 import { expectTypeOf, test } from "vitest";
 
-import { link } from "./link.js";
+import { link, pipe } from "./link.js";
 
 test("allow any source for void target, no fn", () => {
   const evNumber = createEvent<number>();
@@ -23,4 +23,22 @@ test("infer return type from fn", () => {
   const target = link(clock, (p) => p.map((v) => parseInt(v, 10)));
 
   expectTypeOf(target).toMatchTypeOf<Event<number>>();
+});
+
+test("pipe should satisfy both from and target types", async () => {
+  const from = createEvent<number>();
+  const to = createEvent<boolean>();
+
+  link(
+    from,
+    // @ts-expect-error
+    pipe<string>().map(() => true),
+    to,
+  );
+
+  link(
+    from,
+    pipe<number>().map(() => true),
+    to,
+  );
 });
